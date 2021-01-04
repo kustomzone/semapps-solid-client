81ed587b3260207103aa41474718095a67a974a7
Scenaristeur <scenaristeur@gmail.com>
2021-01-04 02:22:38 +0100

    Initial dist subtree commit

---
+++ precache-manifest.9819e7c89849456481c4fd20a7b9c7bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e1929c61e0aad2465d",
+    "url": "/solid-client/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/solid-client/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "56342820832a0c6e48076b5724c373e2",
+    "url": "/solid-client/index.html"
+  },
+  {
+    "revision": "1bb95ff4edc4d76f8fb6",
+    "url": "/solid-client/js/about.a2eaba4f.js"
+  },
+  {
+    "revision": "71e1929c61e0aad2465d",
+    "url": "/solid-client/js/app.99079f07.js"
+  },
+  {
+    "revision": "9a9112111167a0e63aaf",
+    "url": "/solid-client/js/chunk-vendors.de53c0a6.js"
+  },
+  {
+    "revision": "4a2d40c0ae1894a57b1448071d9d904c",
+    "url": "/solid-client/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/solid-client/robots.txt"
+  }
+]);
